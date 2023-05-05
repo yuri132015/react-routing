e838511a5fd05d2f96bcaf8e21fa71bb2af652c4
yuri132015 <42885926+yuri132015@users.noreply.github.com>
2023-05-05 12:58:42 +0900

    Initial commit

---
+++ src/Page1DetailA.jsx
@@ -0,0 +1,13 @@
+import { useHistory } from "react-router-dom";
+
+export const Page1DetailA = () => {
+  const history = useHistory();
+  const onClickBack = () => history.goBack();
+
+  return (
+    <div>
+      <h1>Page1DetailAページです</h1>
+      <button onClick={onClickBack}>戻る</button>
+    </div>
+  );
+};
---
+++ src/UrlParameter.jsx
@@ -0,0 +1,15 @@
+import { useParams, useLocation } from "react-router-dom";
+
+export const UrlParameter = () => {
+  const { id } = useParams();
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+
+  return (
+    <div>
+      <h1>UrlParameterページです</h1>
+      <p>パラメーターは {id} です</p>
+      <p>クエリメーターは {query.get("name")} です</p>
+    </div>
+  );
+};
